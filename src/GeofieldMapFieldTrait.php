8246a831ca07ddff0a4027330eab43b7b9114f20
itamair <itamair@me.com>
2017-09-07 01:54:18 +0200

    map_additional_options-2906936-2.diff applied

--- src/GeofieldMapFieldTrait.php
+++ src/GeofieldMapFieldTrait.php
@@ -628,9 +628,13 @@ trait GeofieldMapFieldTrait {
    * Form element json format validation handler.
    */
   public static function jsonValidate($element, FormStateInterface &$form_state) {
+    $element_values_array = JSON::decode($element['#value']);
     // Check the jsonValue.
-    if (!empty($element['#value']) && JSON::decode($element['#value']) == NULL) {
+    if (!empty($element['#value']) && $element_values_array == NULL) {
       $form_state->setError($element, t('The @field field is not valid Json Format.', ['@field' => $element['#title']]));
+    }
+    else {
+      $form_state->setValueForElement($element, JSON::encode($element_values_array));
     }
   }
 
