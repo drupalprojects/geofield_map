0cf853d3477d61fc26ed7fde33b6efad1d2e58f1
itamair <itamair@me.com>
2018-03-08 01:00:31 +0100

    Body/ "text_with_summary" added as field type for infowindow content.

--- src/GeofieldMapFieldTrait.php
+++ src/GeofieldMapFieldTrait.php
@@ -38,6 +38,7 @@ trait GeofieldMapFieldTrait {
     'string',
     'text',
     'text_long',
+    "text_with_summary",
   ];
 
   protected $customMapStylePlaceholder = '[{"elementType":"geometry","stylers":[{"color":"#1d2c4d"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#8ec3b9"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#1a3646"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"color":"#4b6878"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"color":"#4b6878"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#0e1626"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#4e6d70"}]}]';
